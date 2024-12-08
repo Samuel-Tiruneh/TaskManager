d1b382d9a1fb21c879d034a511cce866f403d7e0
Samuel-Tiruneh <samuelplusweb@gmail.com>
2024-12-08 10:51:39 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const app=express();
+const tasks=require('./routes/tasks');
+const port=3000;
+
+
+app.use(express.json());
+
+app.get('/hello',(req,res)=>{
+    res.send("Task Manager App");
+})
+
+app.use('/api/v1/tasks', tasks);
+
+
+
+app.listen(port,()=>{
+    console.log(`server is running in port ${port}`);
+})
